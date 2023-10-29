e5325c0aef95edda0dc1ac55b6ecba9fabf93051
Blesson <blessonpxavier@gmail.com>
2023-10-29 09:03:18 +0000

    commit Sun 29 Oct 2023 09:03:18 AM UTC

---
+++ buffer_read_test.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class buffer_read_test {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter a String:");
+    String s = br.readLine();
+    System.out.println("You entered:" + s);
+    //br.close();
+
+
+    //BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter a Integer:");
+    int x = Integer.parseInt(br.readLine());
+    System.out.println("You entered:" + x);
+    //br1.close();
+  }
+}
