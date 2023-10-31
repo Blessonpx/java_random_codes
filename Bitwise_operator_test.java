c7d95d0afc65c559de3ac43ee6cf5a5b920cba1d
Blesson <blessonpxavier@gmail.com>
2023-10-31 08:01:57 +0000

    commit Tue 31 Oct 2023 08:01:57 AM UTC

---
+++ Bitwise_operator_test.java
@@ -0,0 +1,8 @@
+class Bitwise_operator_test{
+  public static void main(String[] args){
+    int x=6,y=3;
+    System.out.println(x|y);
+    System.out.println(x^y);
+    System.out.println(x&y);
+  }
+}
